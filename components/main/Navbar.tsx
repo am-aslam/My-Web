c99aa9f2442163cd04222e20537775a9676cb074
muhammed aslam <muhammed.aslam.git@gmail.com>
2025-04-27 20:33:46 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -5,12 +5,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-5 md:px-10">
-      <div className="w-full h-full flex items-center justify-between m-auto">
-        
-        {/* Logo Section */}
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
           href="#about-me"
-          className="flex items-center space-x-2"
+          className="h-auto w-auto flex flex-row items-center"
         >
           <Image
             src="/logo_nav.png"
@@ -19,14 +17,14 @@ const Navbar = () => {
             height={50}
             className="cursor-pointer hover:animate-slowspin"
           />
-          <span className="font-bold hidden md:block text-gray-300">
+
+          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
             AslamSadique
           </span>
         </a>
 
-        {/* Nav Links */}
         <div className="hidden md:flex items-center justify-center">
-          <div className="flex items-center gap-5 border border-[#7042f861] bg-[#0300145e] px-5 py-2 rounded-full text-gray-200">
+          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer text-sm">
               About me
             </a>
@@ -39,20 +37,17 @@ const Navbar = () => {
           </div>
         </div>
 
-        {/* Social Icons */}
-        <div className="flex items-center gap-4">
+        <div className="flex flex-row gap-5">
           {Socials.map((social) => (
-              <Image
-                src={social.src}
-                alt={social.name}
-                width={24}
-                height={24}
-                className="cursor-pointer"
-              />
-            </a>
+            <Image
+              src={social.src}
+              alt={social.name}
+              key={social.name}
+              width={24}
+              height={24}
+            />
           ))}
         </div>
-
       </div>
     </div>
   );
