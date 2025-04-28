71f0a6b7dacd6712ea83e07ef0fa2615579079a4
muhammed aslam <muhammed.aslam.git@gmail.com>
2025-04-28 09:22:24 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           </span>
         </a>
 
-        <div className="hidden md:flex items-center justify-center">
+        <div className="hidden md:flex items-center justify-center xl:w-[500px] h-full flex flex-row items-center justify-between">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer text-sm">
               About me
